ba5df66e5adc4608f70f9f4e9d7b57d37fd677c1
RenatoConfuorto <99622143+RenatoConfuorto@users.noreply.github.com>
2022-03-27 13:17:33 +0200

    6_mp shoot system completed

--- 6_space_invaders/multyplayer/script.js
+++ 6_space_invaders/multyplayer/script.js
@@ -6,6 +6,8 @@ const levelDisplay = document.getElementById('level');
 
 //ARRAY
 const cells = [];
+let aliens = [];
+let aliensKilled = [];
 
 //dimensioni griglia
 const width = 35;
@@ -65,3 +67,98 @@ function moveRedSpaceship(event){
 }
 
 addEventListener('keydown', moveRedSpaceship);
+
+//SPARO
+
+//sparo navicella bianca laser verde
+function greenShoot(event){
+    if(event.code !=='ShiftRight')return;
+    if(event.repeat)return;
+    // console.log(event);
+    //punto partenza del laser
+    let greenLaserIdx = whiteSpaceshipIdx;
+    let greenLaserIntv = null;
+
+    function moveLaser(){
+        cells[greenLaserIdx].classList.remove('green-laser');
+        greenLaserIdx = greenLaserIdx - width;
+        //il laser è uscito dalla griglia
+        if(greenLaserIdx < 0){
+            clearInterval(greenLaserIntv);
+            return;
+        }
+        //controllare se abbiamo colpito l'alieno
+        if(cells[greenLaserIdx].classList.contains('alien')){
+            //abbiamo colpito l'alieno
+            clearInterval(greenLaserIntv);
+
+            //ripulire la cella
+            cells[greenLaserIdx].classList.remove('alien', 'green-laser');
+            cells[greenLaserIdx].classList.add('boom');
+            setTimeout(function(){
+                cells[greenLaserIdx].classList.remove('boom');
+            }, 200);
+
+            //salviamo quali alieni abbiamo ucciso
+            const killed = aliens.indexOf(greenLaserIdx);
+            aliensKilled.push(killed);
+
+            //+1 punto
+            score1++;
+            score1Display.innerText = score1;
+            // checkForLevelUp();
+            return;
+        }
+        cells[greenLaserIdx].classList.add('green-laser');
+    }
+
+    greenLaserIntv =  setInterval(moveLaser, laserSpeed);
+}
+
+document.addEventListener('keydown', greenShoot);
+
+//sparo navicella rossa laser rosso
+function redShoot(event){
+    if(event.code !=='Space')return;
+    if(event.repeat)return;
+    //punto partenza del laser
+    let redLaserIdx = redSpaceshipIdx;
+    let redLaserIntv = null;
+
+    function moveLaser(){
+        cells[redLaserIdx].classList.remove('red-laser');
+        redLaserIdx = redLaserIdx - width;
+        //il laser è uscito dalla griglia
+        if(redLaserIdx < 0){
+            clearInterval(redLaserIntv);
+            return;
+        }
+        //controllare se abbiamo colpito l'alieno
+        if(cells[redLaserIdx].classList.contains('alien')){
+            //abbiamo colpito l'alieno
+            clearInterval(redLaserIntv);
+
+            //ripulire la cella
+            cells[redLaserIdx].classList.remove('alien', 'red-laser');
+            cells[redLaserIdx].classList.add('boom');
+            setTimeout(function(){
+                cells[redLaserIdx].classList.remove('boom');
+            }, 200);
+
+            //salviamo quali alieni abbiamo ucciso
+            const killed = aliens.indexOf(redLaserIdx);
+            aliensKilled.push(killed);
+
+            //+1 punto
+            score2++;
+            score2Display.innerText = score2;
+            // checkForLevelUp();
+            return;
+        }
+        cells[redLaserIdx].classList.add('red-laser');
+    }
+
+    redLaserIntv =  setInterval(moveLaser, laserSpeed);
+}
+
+document.addEventListener('keydown', redShoot);
