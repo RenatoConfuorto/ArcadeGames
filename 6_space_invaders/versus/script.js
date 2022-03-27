6e26402958276da36fc9e4dbd7b3416404f482d5
RenatoConfuorto <99622143+RenatoConfuorto@users.noreply.github.com>
2022-03-27 19:29:59 +0200

    6_versus movement system completed

--- 6_space_invaders/versus/script.js
+++ 6_space_invaders/versus/script.js
@@ -30,3 +30,39 @@ for(let i = 0; i < RxC; i++){
 //NAVICELLA
 cells[whiteSpaceshipIdx].classList.add('white-spaceship');
 cells[redSpaceshipIdx].classList.add('red-spaceship');
+
+//navicella bianca
+function moveWhiteSpaceship(event){
+    const leftEdge = whiteSpaceshipIdx % width === 0;
+    const rightEdge = whiteSpaceshipIdx % width === width - 1;
+    cells[whiteSpaceshipIdx].classList.remove('white-spaceship');
+    // console.log(event);
+    if(event.code === 'ArrowLeft' && !leftEdge){
+        //mi muovo a sinistra
+        whiteSpaceshipIdx--;
+    }else if(event.code === 'ArrowRight' && !rightEdge){
+        //mi muovo a destra
+        whiteSpaceshipIdx++;
+    }
+    cells[whiteSpaceshipIdx].classList.add('white-spaceship');
+}
+
+addEventListener('keydown', moveWhiteSpaceship);
+
+//navicella rossa
+function moveRedSpaceship(event){
+    const leftEdge = redSpaceshipIdx % width === 0;
+    const rightEdge = redSpaceshipIdx % width === width - 1;
+    cells[redSpaceshipIdx].classList.remove('red-spaceship');
+    // console.log(event);
+    if(event.code === 'KeyA' && !leftEdge){
+        //mi muovo a sinistra
+        redSpaceshipIdx--;
+    }else if(event.code === 'KeyD' && !rightEdge){
+        //mi muovo a destra
+        redSpaceshipIdx++;
+    }
+    cells[redSpaceshipIdx].classList.add('red-spaceship');
+}
+
+addEventListener('keydown', moveRedSpaceship);
