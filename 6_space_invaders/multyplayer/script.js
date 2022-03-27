a7ce999c4107239fd93c694fe26705b98950c8f2
RenatoConfuorto <99622143+RenatoConfuorto@users.noreply.github.com>
2022-03-27 12:38:22 +0200

    6_mp dual spaceship movement completed

--- 6_space_invaders/multyplayer/script.js
+++ 6_space_invaders/multyplayer/script.js
@@ -12,6 +12,11 @@ const width = 35;
 const height = 25;
 const RxC = width * height;
 
+//movimento laser e navicelle
+let laserSpeed = 150;
+let whiteSpaceshipIdx = RxC - Math.floor(width/2) - 1;
+let redSpaceshipIdx = RxC - Math.floor(width/2) - width - 1;
+
 //stampare le celle
 for(let i = 0; i < RxC; i++){
     const cell = document.createElement('div');
@@ -20,3 +25,43 @@ for(let i = 0; i < RxC; i++){
     cells.push(cell);
     grid.appendChild(cell);
 }
+
+//NAVICELLA
+cells[whiteSpaceshipIdx].classList.add('white-spaceship');
+cells[redSpaceshipIdx].classList.add('red-spaceship');
+
+//navicella bianca
+function moveWhiteSpaceship(event){
+    const leftEdge = whiteSpaceshipIdx % width === 0;
+    const rightEdge = whiteSpaceshipIdx % width === width - 1;
+    cells[whiteSpaceshipIdx].classList.remove('white-spaceship');
+    // console.log(event);
+    if(event.code === 'ArrowLeft' && !leftEdge){
+        //mi muovo a sinistra
+        whiteSpaceshipIdx--;
+    }else if(event.code === 'ArrowRight' && !rightEdge){
+        //mi muovo a destra
+        whiteSpaceshipIdx++;
+    }
+    cells[whiteSpaceshipIdx].classList.add('white-spaceship');
+}
+
+addEventListener('keydown', moveWhiteSpaceship);
+
+//navicella rossa
+function moveRedSpaceship(event){
+    const leftEdge = redSpaceshipIdx % width === 0;
+    const rightEdge = redSpaceshipIdx % width === width - 1;
+    cells[redSpaceshipIdx].classList.remove('red-spaceship');
+    // console.log(event);
+    if(event.code === 'KeyA' && !leftEdge){
+        //mi muovo a sinistra
+        redSpaceshipIdx--;
+    }else if(event.code === 'KeyD' && !rightEdge){
+        //mi muovo a destra
+        redSpaceshipIdx++;
+    }
+    cells[redSpaceshipIdx].classList.add('red-spaceship');
+}
+
+addEventListener('keydown', moveRedSpaceship);
